c21ac152c952b64608482bce463e88c2f9e6d932
anatolethien <mail@anatolethien.eu>
2024-06-12 11:54:11 +0200

    init script updated with headers dupes removed

--- scripts/init.py
+++ scripts/init.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import requests
+import csv
 
-url = "https://dvf-api.data.gouv.fr/dvf/csv/?section=31555822AB"
-response = requests.get(url)
+response = requests.get("https://dvf-api.data.gouv.fr/dvf/csv/?section=31555822AB")
 
-with open("data/dvf.csv", "wb") as f:
-    f.write(response.content)
+rows = response.content.decode('utf-8').split('\n')
+first_row = rows[0]
+rows = [row for row in rows if row != first_row]
+rows.insert(0, first_row)
+
+rows = [row.split(',') for row in rows]
+
+with open('data/dvf.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerows(rows)
